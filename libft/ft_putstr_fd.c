b3bbc5077fca4e9587f807237d01dbee521cfdb1
Zagaynov Andrew <ngamora@student.21-school.ru>
2021-07-08 18:55:39 +0300

    Added const 2

--- libft/ft_putstr_fd.c
+++ libft/ft_putstr_fd.c
@@ -6,13 +6,13 @@
 /*   By: ngamora <ngamora@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/15 15:53:09 by ngamora           #+#    #+#             */
-/*   Updated: 2021/01/14 16:15:36 by ngamora          ###   ########.fr       */
+/*   Updated: 2021/07/08 17:21:10 by ngamora          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int	ft_putstr_fd(char *s, int fd)
+int	ft_putstr_fd(const char *s, int fd)
 {
 	if (!s)
 		return (0);
