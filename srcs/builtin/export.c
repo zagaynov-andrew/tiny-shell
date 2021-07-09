34447fb8d891c3a97adcf3b2b1439757c30a5a93
Zagaynov Andrew <ngamora@student.21-school.ru>
2021-07-10 00:57:56 +0300

    Processing of all args

--- srcs/builtin/export.c
+++ srcs/builtin/export.c
@@ -6,94 +6,11 @@
 /*   By: ngamora <ngamora@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/06 23:33:23 by ngamora           #+#    #+#             */
-/*   Updated: 2021/07/09 22:51:08 by ngamora          ###   ########.fr       */
+/*   Updated: 2021/07/10 00:08:36 by ngamora          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "builtin.h"
-
-
-void	str_swap(char **str_1, char **str_2)
-{
-	char	*tmp;
-
-	tmp = *str_1;
-	*str_1 = *str_2;
-	*str_2 = tmp;
-}
-
-void	str_array_free(char **str_array)
-{
-	int	i;
-
-	i = 0;
-	while (str_array[i])
-	{
-		// printf("%p\n", str_array[i]);
-		free(str_array[i]);
-		if (i == 110)
-		{
-			i++;
-			i--;
-		}
-		i++;
-	}
-	free(str_array);
-}
-
-size_t	str_array_size(const char **str_array)
-{
-	size_t	size;
-
-	size = 0;
-	while (str_array[size])
-		size++;
-	return (size);
-}
-
-char	**str_array_sort(char **str_array)
-{
-	int	i;
-	int	j;
-	int	size;
-
-	size = (int)str_array_size((const char **)str_array);
-	j = 1;
-	while (j < size)
-	{
-		i = 0;
-		while (i < size - j)
-		{
-			if (ft_strcmp(str_array[i], str_array[i + 1]) > 0)
-				str_swap(&str_array[i], &str_array[i + 1]);
-			i++;
-		}
-		j++;
-	}
-	return (str_array);
-}
-
-char	**str_array_copy(const char **str_array)
-{
-	int		i;
-	int		size;
-	char	**copy;
-
-	size = (int)str_array_size(str_array);
-	copy = (char **)ft_calloc(size + 1, sizeof(char *));
-	if (!copy)
-		return (NULL);
-	i = 0;
-	while (i < size)
-	{
-		copy[i] = ft_strdup(str_array[i]);
-		if (!copy[i])
-			return (NULL);
-		i++;
-	}
-	copy[i] = NULL;
-	return (copy);
-}
 
 void	msh_export_print(const char **str_array)
 {
@@ -163,30 +80,39 @@ int		is_correct_name(const char *env_ptr)
 	return (1);
 }
 
-static char	**str_array_add_back(char **str_array, const char *str)
+int		msh_export_add_env(const char *argv[], char **env[])
 {
-	char	**new;
 	int		i;
-	int		old_size;
+	int		pos;
 
-	if (!str_array)
-		return (NULL);
-	old_size = str_array_size((const char **)str_array);
-	if (!(new = ft_calloc(old_size + 2, sizeof(char*))))
+	i = 1;
+	while (argv[i])
 	{
-		str_array_free((char **)str_array);
-		return (NULL);
-	}
-	i = 0;
-	while (i < old_size)
-	{
-		new[i] = (char *)str_array[i];
+		if (!is_correct_name(argv[i]))
+		{
+			perror("ERROR"); //
+			return (1); //
+		}
+		if ((pos = get_env_pos(argv[i], (const char **)*env)) >= 0)
+		{
+			if (ft_strnstr(argv[i], "=", ft_strlen(argv[i])))
+			{
+				free((char*)(*env)[pos]);
+				(*env)[pos] = ft_strdup(argv[i]);
+			}
+		}
+		else
+		{
+			if (!(*env = str_array_add_back(*env, argv[i])))
+			{
+				perror("ERROR"); //
+				return (errno);
+			}
+		}
+		// print_str_array(*env);
 		i++;
 	}
-	new[i] = ft_strdup(str);
-	new[i + 1] = NULL;
-	free(str_array);
-	return (new);
+	return (0);
 }
 
 int		msh_export(const int argc, const char *argv[], char **env[])
@@ -200,41 +126,18 @@ int		msh_export(const int argc, const char *argv[], char **env[])
 		str_array_sort(copy);
 		msh_export_print((const char **)copy);
 		str_array_free(copy);
+		return (0);
 	}
 	else
-	{
-		int		i = 1; // iteration
-		int		pos;
-
-		if (!is_correct_name(argv[i]))
-		{
-			perror("ERROR"); //
-			return (1); //
-		}
-		if ((pos = get_env_pos(argv[i], (const char **)*env)) >= 0)
-		{
-			if (!ft_strnstr(argv[i], "=", ft_strlen(argv[i])))
-				return (0);
-			free((char*)(*env)[pos]);
-			(*env)[pos] = ft_strdup(argv[i]);
-		}
-		else
-		{
-			if (!(*env = str_array_add_back(*env, argv[i])))
-			{
-				perror("ERROR"); //
-				return (errno);
-			}
-		}
-	}
-	return (0);
+		return (msh_export_add_env(argv, env));
 }
 
 int		main(const int argc, const char *argv[], char *env[])
 {
+	msh_export_print((const char **)env);
 	char	**copy_env = str_array_copy((const char **)env);
 	msh_export(argc, argv, &copy_env);
-	print_str_array(copy_env);
+	msh_export_print((const char **)copy_env);
 	str_array_free(copy_env);
 	return (0);
 }
