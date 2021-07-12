f23bd7ee5be8e79e0bb7b3b2be8c0714aaa2aeda
Zagaynov Andrew <ngamora@student.21-school.ru>
2021-07-12 09:36:07 +0300

    Changing env variables: PWD and OLDPWD

--- srcs/builtin/cd.c
+++ srcs/builtin/cd.c
@@ -6,35 +6,88 @@
 /*   By: ngamora <ngamora@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/06 19:10:41 by ngamora           #+#    #+#             */
-/*   Updated: 2021/07/07 18:13:29 by ngamora          ###   ########.fr       */
+/*   Updated: 2021/07/12 09:31:36 by ngamora          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "builtin.h"
 
-int	msh_cd(int argc, char *argv[], char *env[])
+static int	msh_cd_utils(char *oldpwd, int oldpwd_pos, int pwd_pos, char *env[])
 {
-	(void)argc;
-	(void)argv;
+	char	*pwd;
+
+	pwd = get_cur_dir();
+	if (!oldpwd || !pwd)
+		return (1);
+	if (oldpwd_pos != -1)
+	{
+		free(env[oldpwd_pos]);
+		env[oldpwd_pos] = ft_strjoin("OLDPWD=", oldpwd);
+		free(oldpwd);
+		if (!env[oldpwd_pos])
+			return (1);
+	}
+	if (pwd_pos != -1)
+	{
+		free(env[pwd_pos]);
+		env[pwd_pos] = ft_strjoin("PWD=", pwd);;
+		free(pwd);
+		if (!env[pwd_pos])
+			return (1);
+	}
+}
+
+int	msh_cd(const int argc, const char *argv[], char **env[])
+{
+	int		pwd_pos;
+	int		oldpwd_pos;
+	char	*oldpwd;
+
+	pwd_pos = get_env_pos("PWD", (const char **)*env);
+	oldpwd_pos = get_env_pos("OLDPWD", (const char **)*env);
+	oldpwd = get_cur_dir();
 	if (argc == 1)
 	{
+		
 		if (chdir(getenv("HOME")) != 0)
 			perror("ERROR"); //
+		
 	}
 	else if (argc > 2)
 	{
-		perror("too many arguments"); //
+		perror("too many arguments"); // return value
 	}
 	else
 	{
 		if (chdir(argv[1]) != 0)
-			perror("ERROR"); //
+			return (1);
 	}
-	return (0);
+	return (msh_cd_utils(oldpwd, oldpwd_pos, pwd_pos, *env));
 }
 
 // int main(int argc, char *argv[], char *env[])
 // {
-//     msh_cd(argc, argv, env);
+// 	char	**copy_env;
+
+	
+
+// 	copy_env = NULL;
+// 	copy_env = str_array_copy((const char **)env);
+// 	// str_array_ffree(copy_env);
+// 	// copy_env = str_array_add_back(&copy_env, "pwd=string");
+// 	// copy_env = str_array_add_back(&copy_env, "second=string");
+// 	// copy_env = str_array_add_back(&copy_env, "OLDPWD=string");
+// 	// copy_env = str_array_add_back(&copy_env, "PWD=string");
+// 	// copy_env = str_array_add_back(&copy_env, "third=string");
+// 	// copy_env = str_array_add_back(&copy_env, "forth=string");
+//     msh_cd((const int)argc, (const char **)argv, &copy_env);
+// 	char	*cur_dir = get_cur_dir();
+// 	printf("PWD = %s\n", cur_dir);
+// 	int	oldpwd_pos = get_env_pos("OLDPWD", (const char **)copy_env);
+// 	int	pwd_pos = get_env_pos("PWD", (const char **)copy_env);
+// 	printf("%s\n", copy_env[oldpwd_pos]);
+// 	printf("%s\n", copy_env[pwd_pos]);
+// 	free(cur_dir);
+// 	str_array_free(copy_env);
 //     return (0);
 // }
