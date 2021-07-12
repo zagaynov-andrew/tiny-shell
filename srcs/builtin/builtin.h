9fed1fcce3605b53581d9acc56b885d06cd95ffc
Zagaynov Andrew <ngamora@student.21-school.ru>
2021-07-12 09:34:24 +0300

    Fixed to getcwd

--- srcs/builtin/builtin.h
+++ srcs/builtin/builtin.h
@@ -6,7 +6,7 @@
 /*   By: ngamora <ngamora@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/06 22:07:23 by ngamora           #+#    #+#             */
-/*   Updated: 2021/07/11 13:28:05 by ngamora          ###   ########.fr       */
+/*   Updated: 2021/07/12 00:41:34 by ngamora          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,12 +19,14 @@
 # include <stdlib.h>
 # include <unistd.h>
 
-int	msh_echo(int argc, char *argv[], char *env[]);
-int	msh_pwd(int argc, char *argv[], char *env[]);
-int	msh_cd(int argc, char *argv[], char *env[]);
-int	msh_env(int argc, char *argv[], char *env[]);
-int	msh_export(const int argc, const char *argv[], char **env[]);
-int	is_valid_env_name(const char *env_ptr);
-int	get_env_pos(const char *name, const char **env);
+int		msh_echo(int argc, char *argv[], char *env[]);
+int		msh_pwd(int argc, char *argv[], char *env[]);
+int		msh_cd(const int argc, const char *argv[], char **env[]);
+int		msh_env(int argc, char *argv[], char *env[]);
+int		msh_export(const int argc, const char *argv[], char **env[]);
+int		is_valid_env_name(const char *env_ptr);
+int		get_env_pos(const char *name, const char **env);
+char	*get_env_var_value(const char *name, const char **env);
+char	*get_cur_dir(void);
 
 #endif
--- srcs/builtin/pwd.c
+++ srcs/builtin/pwd.c
@@ -6,14 +6,44 @@
 /*   By: ngamora <ngamora@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/06 20:05:29 by ngamora           #+#    #+#             */
-/*   Updated: 2021/07/11 13:33:31 by ngamora          ###   ########.fr       */
+/*   Updated: 2021/07/12 09:30:22 by ngamora          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "builtin.h"
 
+char	*get_cur_dir(void)
+{
+	int		size;
+	char	*path;
+	char	*tmp;
+
+	size = 64;
+	path = (char *)ft_calloc(size, sizeof(char));
+	if (!path)
+		return (NULL);
+	tmp = path;
+	path = getcwd(path, size);
+	if (!path && errno != 34)
+		return (path);
+	while (!path)
+	{
+		free(tmp);
+		size *= 2;
+		path = (char *)ft_calloc(size, sizeof(char));
+		if (!path)
+			return (NULL);
+		path = getcwd(path, size);
+		if (!path && errno != 34)
+			return (path);
+	}
+	errno = 0;
+	return (path);
+}
+
 int	msh_pwd(int argc, char *argv[], char *env[])
 {
+	char	*cur_dir;
 	(void)env;
 	if (argc > 1)
 	{
@@ -30,7 +60,11 @@ int	msh_pwd(int argc, char *argv[], char *env[])
 			return (0);
 		}
 	}
-	printf("%s\n", getenv("PWD"));
+	cur_dir = get_cur_dir();
+	if (!cur_dir)
+		return (1);
+	printf("%s\n", cur_dir);
+	free(cur_dir);
 	return (0);
 }
 
