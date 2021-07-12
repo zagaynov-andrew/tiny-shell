c55d77bfe648c2b2e51046878a11cdcaaf9838e3
Zagaynov Andrew <ngamora@student.21-school.ru>
2021-07-12 21:50:43 +0300

    Error output: invalid option

--- srcs/builtin/pwd.c
+++ srcs/builtin/pwd.c
@@ -6,7 +6,7 @@
 /*   By: ngamora <ngamora@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/06 20:05:29 by ngamora           #+#    #+#             */
-/*   Updated: 2021/07/12 14:20:45 by ngamora          ###   ########.fr       */
+/*   Updated: 2021/07/12 18:19:00 by ngamora          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -50,14 +50,18 @@ int	msh_pwd(int argc, char *argv[], char *env[])
 		if (argv[1][0] == '-' && argv[1][1] == '-'
 			&& argv[1][2] != '\0') // check -, --, -dafb, -s
 		{
-			perror("-q: invalid option"); //
-			return (0);
+			ft_putstr_fd("minishell: pwd: -", 2);
+			ft_putchar_fd(argv[1][1], 2);
+			ft_putstr_fd(": invalid option\n", 2);
+			return (2);
 		}
 		else if (argv[1][0] == '-' && argv[1][1] != '\0'
 			&& argv[1][1] != '-') // check -, --, -dafb, -s
 		{
-			perror("-q: invalid option"); //
-			return (0);
+			ft_putstr_fd("minishell: pwd: -", 2);
+			ft_putchar_fd(argv[1][1], 2);
+			ft_putstr_fd(": invalid option\n", 2);
+			return (2);
 		}
 	}
 	cur_dir = get_cur_dir();
